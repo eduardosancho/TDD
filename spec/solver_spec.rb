d38228cd7959d8a2bcafae6ee514daba60d822f2
Reem <janinareem@gmail.com>
2022-04-15 00:16:41 +0200

    Add the tests for raiseing an exception

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,14 +9,19 @@ describe 'Solver' do
       expect(solver1).to receive(:factorial).with(3).ordered.and_call_original
       expect(solver1).to receive(:factorial).with(2).ordered.and_call_original
       expect(solver1).to receive(:factorial).with(1).ordered.and_call_original
-      expect(solver1).to receive(:factorial).with(0).ordered.and_call_original
       solver1.factorial(3)
     end
 
-    it 'Test the result that it takes integer and return its factorial' do
+    it 'Test the result that it takes integer and return its factorial:' do
       solver2 = Solver.new
       expect(solver2.factorial(4)).to eq(24)
       expect(solver2.factorial(5)).to eq(120)
+    end
+
+    it 'Test should raise an exception if the argument is a negative number:' do
+      solver3 = Solver.new
+      expect { solver3.factorial(-1) }.to raise_exception(Exception)
+      expect { solver3.factorial(-5) }.to raise_exception(Exception)
     end
   end
 end
