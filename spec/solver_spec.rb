18106faa9eba5438c3dd356315add286fe946c80
Reem <janinareem@gmail.com>
2022-04-14 23:45:02 +0200

    Add tests for factorial function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
-class Solver
-  def factorial
-    number * 2
-  end
-end
+require_relative '../solver'
 
-describe 'Outer' do
+describe 'Solver' do
+  context 'Factorial tests' do
+    it 'the function factorial called several times by recursion' do
+      solver1 = Solver.new
+      expect(solver1).to receive(:factorial).with(3).ordered.and_call_original
+      expect(solver1).to receive(:factorial).with(2).ordered.and_call_original
+      expect(solver1).to receive(:factorial).with(1).ordered.and_call_original
+      expect(solver1).to receive(:factorial).with(0).ordered.and_call_original
+      solver1.factorial(3)
+    end
 
-  it 'the factorial method should work correctly' do
-    solver1 = Solver.new
-
-    allow(solver1).to receive(:number).and_return(3)
-
-    expect(solver1.factorial).to eql(6)
+    it 'Test the result that it takes integer and return its factorial' do
+      solver2 = Solver.new
+      expect(solver2.factorial(4)).to eq(24)
+      expect(solver2.factorial(5)).to eq(120)
+    end
   end
 end
