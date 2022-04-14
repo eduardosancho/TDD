694ad715203a7950368c5182c8a631883d1906a1
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-14 16:55:54 -0600

    ADD FUNCTION fizzbuzz

--- solver.rb
+++ solver.rb
@@ -10,4 +10,16 @@ class Solver
   def reverse_str(str)
      str.split('').reverse.join('')
   end
+
+  def fizzbuz(n)
+    if n%3 == 0 && n%5 == 0 
+      return 'fizzbuzz'
+    elsif n%3 == 0
+      return 'fizz'
+    elsif n%5 == 0
+      return 'buzz'
+    else
+      return n.to_s
+    end
+  end
 end
