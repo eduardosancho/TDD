398167956bf5a8db11b35f4618054fa74a5f2569
Reem <janinareem@gmail.com>
2022-04-15 00:35:40 +0200

    Add fizzbuzz tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -33,5 +33,31 @@ describe 'Solver' do
     end
   end
 
-  
+  context 'Test Fizzbuzz algorithm' do
+    it 'the function returns fizzbuzz if the number is divisble by 3 and 5' do
+      solver5 = Solver.new
+      expect(solver5.fizzbuz(15)).to eq('fizzbuzz')
+      expect(solver5.fizzbuz(30)).to eq('fizzbuzz')
+    end
+
+    it 'the function returns fizz if the number is divisble 3' do
+      solver6 = Solver.new
+      expect(solver6.fizzbuz(9)).to eq('fizz')
+      expect(solver6.fizzbuz(3)).to eq('fizz')
+    end
+
+    it 'the function returns buzz if the number is divisble by 5' do
+      solver7 = Solver.new
+      expect(solver7.fizzbuz(5)).to eq('buzz')
+      expect(solver7.fizzbuz(25)).to eq('buzz')
+    end
+
+    
+    it 'the function returns the number if not divisvle by 3 or 5' do
+      solver8 = Solver.new
+      expect(solver8.fizzbuz(8)).to eq('8')
+      expect(solver8.fizzbuz(19)).to eq('19')
+    end
+  end
+
 end
